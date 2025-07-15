ab666ce49989f0b93ba6e5db6f4c90d415c014c0
HarshalRawal <harshalrawal463@gmail.com>
2025-07-15 17:08:16 +0530

    testing ngrok

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -2,11 +2,11 @@ import axios from "axios"
 
 // Create axios instance with base URL
 export const axiosInstance = axios.create({
-  baseURL: "http://13.203.74.174:4000/api/v1",
+  baseURL: "https://9fbf264f7bb4.ngrok-free.app/api/v1",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials : true
-})
+  withCredentials: true
+});
 
