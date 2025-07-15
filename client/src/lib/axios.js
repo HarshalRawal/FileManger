b1ed5bfb752c6da6a2d9947751c8bb1fc67851a4
HarshalRawal <harshalrawal463@gmail.com>
2025-07-15 17:25:48 +0530

    changed axios setting

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 // Create axios instance with base URL
 export const axiosInstance = axios.create({
-  baseURL: "https://9fbf264f7bb4.ngrok-free.app/api/v1",
+  baseURL:  "https://d4822f116923.ngrok-free.app/api/v1",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
