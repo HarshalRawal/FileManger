1e5deb6d10015a0cc231913d57484f2340064d2f
HarshalRawal <harshalrawal463@gmail.com>
2025-07-15 16:44:14 +0530

    changed the axios base url

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 // Create axios instance with base URL
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: "http://13.203.74.174:4000/api/v1",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
